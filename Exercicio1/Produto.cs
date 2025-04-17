0af7f9598ba356e333a432822040e0a76a387877
graziella.pereira <graziellapereira52@gmail.com>
2025-04-17 17:50:39 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Produto
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { private get; set; }
+        public int QuantidadeTotal { get; private set; } = 0;
+
+        public void AdicionarEstoque()
+        {
+            QuantidadeTotal += Quantidade;  
+            Console.WriteLine("Produtos adicionados!");
+        }
+
+        public void RemoverEstoque(int qtde)
+        {
+            if (qtde <= QuantidadeTotal)
+            {
+                QuantidadeTotal -= qtde;
+                Console.WriteLine("Produtos removidos!");
+            }
+            else
+            {
+                Console.WriteLine("Não é possível remover essa quantidade de produtos. (Quantia maior do que a presente no estoque.");
+            }
+        }
+        public void ValorTotalEmEstoque()
+        {
+            double precoTotal;
+            precoTotal = Preco * QuantidadeTotal;
+            Console.WriteLine($"Preço total: R${precoTotal: F2}");
+        }
+    }
+}
