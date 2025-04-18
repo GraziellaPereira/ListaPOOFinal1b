347d7884ed593a5d6c43e2376606db7ecd5a3975
GraziellaPereira <graziellapereira52@gmail.com>
2025-04-18 19:49:35 -0300

    Terminado exercício 1

--- Exercicio1/Produto.cs
+++ Exercicio1/Produto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Produto
+namespace ProdutoClasse
 {
     public class Produto
     {
@@ -8,11 +9,19 @@ namespace Produto
         public double Preco { get; set; }
         public int Quantidade { private get; set; }
         public int QuantidadeTotal { get; private set; } = 0;
+        public static int QuantidadeTotalGeral { get; private set; } = 0;
+        public static double PrecoTotalGeral { get; private set; } = 0;
 
         public void AdicionarEstoque()
         {
-            QuantidadeTotal += Quantidade;  
-            Console.WriteLine("Produtos adicionados!");
+            Console.WriteLine("Informe a quantidade a adicionar: ");
+            Quantidade = int.Parse(Console.ReadLine());
+            double PrecoTotal = Preco * Quantidade;
+            PrecoTotalGeral += PrecoTotal;
+            QuantidadeTotal += Quantidade;
+            QuantidadeTotalGeral += Quantidade;
+            Console.WriteLine($"{Quantidade} {Nome}(s) adicionados(as). Pressione qualquer tecla para continuar.");
+            Console.ReadKey();
         }
 
         public void RemoverEstoque(int qtde)
@@ -20,18 +29,36 @@ namespace Produto
             if (qtde <= QuantidadeTotal)
             {
                 QuantidadeTotal -= qtde;
-                Console.WriteLine("Produtos removidos!");
+                QuantidadeTotalGeral -= qtde;
+                PrecoTotalGeral -= qtde * Preco;
+                Console.WriteLine($"{qtde} {Nome}(s) removidos(as). Pressione qualquer tecla para continuar.");
+                Console.ReadKey();
             }
             else
             {
-                Console.WriteLine("Não é possível remover essa quantidade de produtos. (Quantia maior do que a presente no estoque.");
+                Console.WriteLine($"Não é possível remover {qtde} {Nome}(s). (Quantia informada maior do que a presente no estoque.). Pressione qualquer tecla para continuar.");
+                Console.ReadKey();
             }
+        }
+
+        public static Produto BuscarProduto(List<Produto> produtos, string nome)
+        {
+            return produtos.Find(p => p.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase));
         }
         public void ValorTotalEmEstoque()
         {
-            double precoTotal;
-            precoTotal = Preco * QuantidadeTotal;
-            Console.WriteLine($"Preço total: R${precoTotal: F2}");
+            double total = Preco * QuantidadeTotal;
+            Console.WriteLine($"\n Preço total de {Nome} = R${total:F2}. Pressione qualquer tecla para continuar.");
+            Console.ReadKey();
+
         }
+        public static void MostrarTotaisGerais()
+        {
+            Console.WriteLine($"\nQuantidade total do estoque: {QuantidadeTotalGeral}");
+            Console.WriteLine($"Valor total do estoque: R$ {PrecoTotalGeral:F2}. Pressione qualquer tecla para continuar.");
+            Console.ReadKey();
+        }
+
+
     }
 }
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ProdutoClasse;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+            bool executando = true;
+
+            Console.WriteLine("Bem-vindo ao sistema de estoque de produtos!");
+
+            while (executando)
+            {
+                Console.Clear();
+                Console.WriteLine("\nO que deseja fazer agora?");
+                Console.WriteLine("1 - Adicionar produto ao estoque");
+                Console.WriteLine("2 - Remover produto do estoque");
+                Console.WriteLine("3 - Verificar o preço total de um produto no estoque");
+                Console.WriteLine("4 - Ver valor total geral do estoque");
+                Console.WriteLine("5 - Sair");
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o nome do produto: ");
+                        string nomeAdd = Console.ReadLine();
+
+                        Produto produtoAdd = Produto.BuscarProduto(produtos, nomeAdd);
+                        if (produtoAdd == null)
+                        {
+                            produtoAdd = new Produto();
+                            produtoAdd.Nome = nomeAdd;
+                            Console.WriteLine($"Digite o preço unitário de {nomeAdd}: ");
+                            produtoAdd.Preco = double.Parse(
+                            Console.ReadLine().Replace(',', '.'),
+                            CultureInfo.InvariantCulture
+                            );
+                        }
+
+                        produtoAdd.AdicionarEstoque();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Digite o nome do produto: ");
+                        string nomeRemove = Console.ReadLine();
+
+                        Produto produtoRemove = Produto.BuscarProduto(produtos, nomeRemove);
+                        if (produtoRemove == null)
+                        {
+                            Console.WriteLine("Produto não encontrado!");
+                            break;
+                        }
+
+                        Console.WriteLine($"Digite a quantidade a remover de {produtoRemove.Nome}: ");
+                        int qtdeRemove = int.Parse(Console.ReadLine());
+                        produtoRemove.RemoverEstoque(qtdeRemove);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Digite o nome do produto: ");
+                        string nomeConsulta = Console.ReadLine();
+
+                        Produto produtoConsulta = Produto.BuscarProduto(produtos, nomeConsulta);
+                        if (produtoConsulta == null)
+                        {
+                            Console.WriteLine("Produto não encontrado!");
+                        }
+                        else
+                        {
+                            produtoConsulta.ValorTotalEmEstoque();
+                        }
+                        break;
+
+                    case 4:
+                        Produto.MostrarTotaisGerais();
+                        break;
+
+                    case 5:
+                        executando = false;
+                        Console.WriteLine("Sistema encerrado.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Digite um número de 1 a 5!");
+                        break;
+                }
+            }
+        }
+    }
+}
